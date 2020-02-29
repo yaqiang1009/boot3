4be42e29bcaeb4956303772d8e210598c0a190e8
gyq <gyq@qq.com>
2020-02-29 19:57:21 +0800

    ins2

--- src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
+++ src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
@@ -2,10 +2,11 @@ package com.wnxy.springboot.myspringboot03.service;
 
 public class service1 {
 	public void b() {
-		System.out.println(0);
+		System.out.println(1);
 }
 	public void a() {
 		System.out.println(11);
-		System.out.println(12);
+		System.out.println(0);
+		System.out.println(11);
 	}
 }
