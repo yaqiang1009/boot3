7493e895b49301e85617229ea9c6989b17721391
gyq <gyq@qq.com>
2020-02-29 19:55:30 +0800

    0

--- src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
+++ src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
@@ -2,7 +2,7 @@ package com.wnxy.springboot.myspringboot03.service;
 
 public class service1 {
 	public void b() {
-		System.out.println(0);
+		System.out.println(2);
 }
 	public void a() {
 		System.out.println(11);
--- src/main/java/com/wnxy/springboot/myspringboot03/service/service2.java
+++ src/main/java/com/wnxy/springboot/myspringboot03/service/service2.java
@@ -3,5 +3,6 @@ package com.wnxy.springboot.myspringboot03.service;
 public class service2 {
 	public void b() {
 		System.out.println(0);
+		System.out.println(1);
 	}
 }
