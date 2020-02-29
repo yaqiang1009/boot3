9dace3d0fc200c25ea4a44debdbd859a6742abb6
gyq <gyq@qq.com>
2020-02-29 19:47:21 +0800

    insert 3

--- src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
+++ src/main/java/com/wnxy/springboot/myspringboot03/service/service1.java
@@ -3,6 +3,6 @@ package com.wnxy.springboot.myspringboot03.service;
 public class service1 {
 	public void a() {
 		System.out.println(11);
-		System.out.println(12);
+		System.out.println(111);
 	}
 }
